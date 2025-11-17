08d6dfe7e2188a25d93127133e301a8a1fe9d2a0
kouamdo <kouamdoux@gmail.com>
2025-11-18 01:57:07 +0300

    add matrix functions

---
+++ lesson-05/src/homework/include/Matrix.hpp
@@ -0,0 +1,43 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <cstdint>
+#include <stdexcept>
+#include <iostream>
+
+class Matrix
+{
+private:
+    class ProxyRow
+    {
+    private:
+        int32_t* data_;
+        size_t cols_;
+    public:
+        ProxyRow();
+        ProxyRow(int32_t* row_data, size_t cols);
+        int32_t& operator[](size_t j);
+    };
+
+    int32_t** data;
+    size_t rows;
+    size_t cols;
+    ProxyRow* rows_data;
+
+public:
+    Matrix(size_t rows, size_t cols);
+    ~Matrix();
+
+    int32_t& at(size_t i, size_t j);
+
+    Matrix& operator*=(int32_t val);
+    Matrix operator+(Matrix& other) const;
+    bool operator==(Matrix& other) const;
+    bool operator!=(Matrix& other) const;
+
+    ProxyRow& operator[](size_t i);
+
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
+};
+
+#endif // MATRIX_HPP
---
+++ lesson-05/src/homework/src/Matrix.cpp
@@ -0,0 +1,102 @@
+#include "../include/Matrix.hpp"
+
+// ProxyRow
+
+Matrix::ProxyRow::ProxyRow() : data_(nullptr), cols_(0) {}
+
+Matrix::ProxyRow::ProxyRow(int32_t* row_data, size_t cols) : data_(row_data), cols_(cols) {}
+
+int32_t& Matrix::ProxyRow::operator[](size_t j)
+{
+    if (j >= cols_)
+        throw std::out_of_range("Column index out of range");
+    return data_[j];
+}
+
+// Matrix
+
+Matrix::Matrix(size_t r, size_t c) : rows(r), cols(c)
+{
+    data = new int32_t*[rows];
+    for (size_t i = 0; i < rows; i++)
+        data[i] = new int32_t[cols]();
+
+    rows_data = new Matrix::ProxyRow[rows];
+    for (size_t j = 0; j < rows; j++)
+        rows_data[j] = ProxyRow(data[j], cols);
+}
+
+Matrix::~Matrix()
+{
+    for (size_t i = 0; i < rows; i++)
+        delete[] data[i];
+    delete[] data;
+    delete[] rows_data;
+}
+
+int32_t& Matrix::at(size_t i, size_t j)
+{
+    if (i >= rows || j >= cols)
+        throw std::out_of_range("Matrix indice out of range");
+    return data[i][j];
+}
+
+Matrix& Matrix::operator*=(int32_t val)
+{
+    for (size_t i = 0; i < rows; i++)
+        for (size_t j = 0; j < cols; j++)
+            data[i][j] *= val;
+    return *this;
+}
+
+Matrix Matrix::operator+(Matrix& other) const
+{
+    if (other.rows == rows && other.cols == cols)
+    {
+        Matrix result(rows, cols);
+        for (size_t i = 0; i < rows; i++)
+            for (size_t j = 0; j < cols; j++)
+                result.data[i][j] = data[i][j] + other.data[i][j];
+        return result;
+    }
+    else
+        throw std::invalid_argument("Matrices sizes do not match");
+}
+
+bool Matrix::operator==(Matrix& other) const
+{
+    if (other.rows == rows && other.cols == cols)
+    {
+        for (size_t i = 0; i < rows; i++)
+            for (size_t j = 0; j < cols; j++)
+                if (data[i][j] != other.data[i][j])
+                    return false;
+
+        return true;
+    }
+    else
+        throw std::invalid_argument("Matrices sizes do not match");
+}
+
+bool Matrix::operator!=(Matrix& other) const
+{
+    return !(*this == other);
+}
+
+Matrix::ProxyRow& Matrix::operator[](size_t i)
+{
+    if (i >= rows)
+        throw std::out_of_range("Row index out of range");
+    return rows_data[i];
+}
+
+std::ostream& operator<<(std::ostream& os, const Matrix& m)
+{
+    for (size_t i = 0; i < m.rows; i++)
+    {
+        for (size_t j = 0; j < m.cols; j++)
+            os << m.data[i][j] << ' ';
+        os << '\n';
+    }
+    return os;
+}
