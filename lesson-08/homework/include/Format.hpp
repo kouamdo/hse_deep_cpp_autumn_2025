5a66821654e07d29fea2b4ac181b6bf3442f38c1
kouamdo <kouamdoux@gmail.com>
2025-11-25 18:19:23 +0300

    Add format implement

--- lesson-08/homework/include/Format.hpp
+++ lesson-08/homework/include/Format.hpp
@@ -4,6 +4,10 @@
 #include <cstdint>
 #include <stdexcept>
 #include <iostream>
+#include <tuple>
+#include <sstream>
+#include <stdexcept>
+#include <string>
 
 #pragma once
 
@@ -18,6 +22,40 @@ class Format
 
     public:
 
+        template<template T>
+        std::string argToSrtring(const T& value) const
+        {
+            std::stringstream ss ; 
+
+            ss << value ;
+
+            return ss.str();
+        }
+
+        template<std::size_t Index>
+        std::string getArgByIndex() const {
+            if constexpr (Index >= sizeof...(Args))
+                throw std::out_of_range("Indice argument hors limites");
+            else
+                return argToString(std::get<Index>(args));
+        }
+
+        std::string getArg(std::size_t index) const {
+        // On doit dispatcher l’index à la bonne surcharge getArgByIndex
+        // Comme std::get requiert un index en constexpr, ici une implémentation simple :
+            switch (index) {
+                #define CASE(i) case i: return getArgByIndex<i>();
+                CASE(0)
+                CASE(1)
+                CASE(2)
+                CASE(3)
+                CASE(4)
+                // Ajouter autant de cases que le nombre maximum d’arguments attendus
+                #undef CASE
+                default: throw std::out_of_range("Argument index hors limites");
+            }
+        }
+        
         
         Format(const std::string& fmt, Args&&... arguments)
         : format_str(fmt),
---
+++ lesson-08/homework/tests/format_test.cpp
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include "Format.h"  // Votre classe Format ou fonction format
+
+// Cas simple : un argument
+TEST(FormatTest, Basic) {
+    Format<int> f("Bonjour {0}!", 42);
+    f.parse();
+    EXPECT_EQ(f.str(), "Bonjour 42!");
+}
+
+// Répétition d’indices
+TEST(FormatTest, RepeatIndices) {
+    Format<int, std::string> f("{1}+{1}={0}", 2, "one");
+    f.parse();
+    EXPECT_EQ(f.str(), "one+one=2");
+}
+
+// Erreur : accolades vides
+TEST(FormatTest, EmptyBraces) {
+    EXPECT_THROW({
+        Format<> f("{}");
+        f.parse();
+    }, FormatError);
+}
+
+// Erreur : caractère non numérique dans accolades
+TEST(FormatTest, InvalidIndex) {
+    EXPECT_THROW({
+        Format<> f("{a}");
+        f.parse();
+    }, FormatError);
+}
+
+// Erreur : accolade ouvrante non fermée
+TEST(FormatTest, UnclosedBrace) {
+    EXPECT_THROW({
+        Format<> f("{0");
+        f.parse();
+    }, FormatError);
+}
+
+// Erreur : accolade fermante sans ouvrante
+TEST(FormatTest, ClosingWithoutOpening) {
+    EXPECT_THROW({
+        Format<> f("text }");
+        f.parse();
+    }, FormatError);
+
+
+// Erreur : indice hors borne
+TEST(FormatTest, IndexOutOfRange) {
+    EXPECT_THROW({
+        Format<int> f("{1}", 42);  // Seulement un argument (indice 0), demande 1
+        f.parse();
+    }, std::out_of_range);
+}
